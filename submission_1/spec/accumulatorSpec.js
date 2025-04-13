67eae21e3914c3c7e55d808359c0c73b08484ff0
Aria Fatah <ariafatah0711@gmail.com>
2025-04-13 22:35:55 +0700

    update

--- submission_1/spec/accumulatorSpec.js
+++ submission_1/spec/accumulatorSpec.js
@@ -1,31 +1,31 @@
-describe('accumulator', function () {
+describe("accumulator", function () {
+  beforeEach(angular.mock.module("accumulatorApp"));
 
-	beforeEach(angular.mock.module('accumulatorApp'));
+  var $controller, $httpBackend;
 
-	var $controller, $httpBackend;
+  beforeEach(
+    angular.mock.inject(function (_$controller_, _$httpBackend_) {
+      $controller = _$controller_;
+      $httpBackend = _$httpBackend_;
+    })
+  );
 
-	beforeEach(angular.mock.inject(function (_$controller_, _$httpBackend_) {
-		$controller = _$controller_;
-		$httpBackend = _$httpBackend_;
-	}));
+  afterEach(function () {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
 
-	afterEach(function () {
-		$httpBackend.verifyNoOutstandingExpectation();
-		$httpBackend.verifyNoOutstandingRequest();
-	});
+  describe("Messages", function () {
+    it("add increases length by one", function () {
+      var $scope = {};
+      var controller = $controller("AccumulatorController", { $scope: $scope });
+      $httpBackend.expectPOST("/api").respond(201, "{}");
 
-	describe('Messages', function () {
-		it('add increases length by one', function () {
-			var $scope = {};
-			var controller = $controller('AccumulatorController', { $scope: $scope });
-			$httpBackend.expectPOST('/api').respond(201, '{}');
+      $scope.item = "newItem";
+      $scope.add();
+      $httpBackend.flush();
 
-			$scope.item = "newItem";
-			$scope.add();
-			$httpBackend.flush();
-
-			expect($scope.accumulator.length).toBe(1);
-		});
-	});
-
+      expect($scope.accumulator.length).toBe(1);
+    });
+  });
 });
--- submission_2/README.md
+++ submission_2/README.md
@@ -1,10 +1,17 @@
 ```bash
+# show
+kubectl get pv,pvc,secret,configmap -o wide
+kubectl get all -o wide -n submission2
+
+# create
 kubectl create namespace submission2
 kubectl apply -f kubernetes/mongodb/
 kubectl apply -f kubernetes/backend/
 kubectl apply -f kubernetes/frontend/
-kubectl get all,pv,pvc,secret,configmap -o wide
+kubectl get pv,pvc,secret,configmap -o wide
+kubectl get all -o wide -n submission2
 
+# delete
 kubectl delete -f kubernetes/mongodb/
 kubectl delete -f kubernetes/backend/
 kubectl delete -f kubernetes/frontend/
