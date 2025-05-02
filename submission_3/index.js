b9e9387cf03353afa934bd289ce5f10edadd65a2
Aria Fatah <ariafatah0711@gmail.com>
2025-05-03 00:56:41 +0700

    update

--- submission_3/index.js
+++
@@ -1,9 +0,0 @@
-const amqp = require('amqplib');
-(async () => {
-  try {
-    const conn = await amqp.connect('amqp://admin:admin123@192.168.49.2:30006');
-    console.log('✅ Connected!');
-  } catch (err) {
-    console.error(err);
-  }
-})();
